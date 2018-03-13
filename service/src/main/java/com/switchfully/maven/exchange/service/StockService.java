e692ab9c7bf16144a91dfb9edf1738f33a5acfce
nielsdelestinne <nielsdelestinne@gmail.com>
2018-03-13 10:04:12 +0100

    Added logging!

--- service/src/main/java/com/switchfully/maven/exchange/service/StockService.java
+++ service/src/main/java/com/switchfully/maven/exchange/service/StockService.java
@@ -7,14 +7,19 @@ import com.switchfully.maven.exchange.domain.StockRepository;
 import com.switchfully.maven.exchange.interfaces.ExternalStockInformationService;
 
 import java.math.BigDecimal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class StockService {
+
+    private final static Logger LOGGER = Logger.getLogger(StockService.class.getName());
 
     public Stock getStock(String stockId) {
         try {
             Stock stockWithoutPrice = StockRepository.getStockInformation(stockId);
             return enrichtStockWithPrice(stockWithoutPrice);
         } catch (IllegalArgumentException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage());
             return new Stock(stockId, "Unknown stock");
         }
     }
